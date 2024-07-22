e8f2c0e26856ae07aa5edbbb757a64d0c9369607
ugurkocak1980@gmail.com <ugurkocak1980@gmail.com!>
2024-07-22 15:07:53 +0200

    fixed postgres running in docker container & flyway

--- src/main/resources/db/migration/V1.0__Task.sql
+++ src/main/resources/db/migration/V1.0__Task.sql
@@ -1,9 +1,9 @@
-CREATE TYPE tasks.prio_enum AS ENUM ('LOW', 'NORMAL', 'URGENT');
-CREATE CAST (varchar as tasks.prio_enum) WITH INOUT AS IMPLICIT;
-CREATE TABLE tasks.task (
+CREATE TYPE prio_enum AS ENUM ('LOW', 'NORMAL', 'URGENT');
+CREATE CAST (varchar as prio_enum) WITH INOUT AS IMPLICIT;
+CREATE TABLE task (
    id serial primary key,
    name VARCHAR(255),
    done BOOLEAN,
    created TIMESTAMP WITHOUT TIME ZONE,
-   priority tasks.prio_enum
+   priority prio_enum
 );
